a1d55557b8e186151ab5dbca778cc2d06c1b1c35
Jarek Warowny <jwarowny99@gmail.com>
2022-04-12 23:39:01 +0200

    22.2

---
+++ src/main/java/com/crud/tasks/trello/client/TrelloClient.java
@@ -0,0 +1,45 @@
+package com.crud.tasks.trello.client;
+
+
+import com.crud.tasks.domain.TrelloBoardDto;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+import org.springframework.web.client.RestTemplate;
+import org.springframework.web.util.UriComponentsBuilder;
+
+import java.net.URI;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+import java.util.Optional;
+
+@Component
+@RequiredArgsConstructor
+public class TrelloClient {
+
+    private final RestTemplate restTemplate;
+
+    @Value("${trello.api.endpoint.prod}")
+    private String trelloApiEndpoint;
+    @Value("${trello.api.key}")
+    private String trelloAppKey;
+    @Value("${trello.api.token}")
+    private String treloToken;
+
+    public List<TrelloBoardDto> getTrelloBoards() {
+        URI url = UriComponentsBuilder.fromHttpUrl(trelloApiEndpoint + "/boards/623759001840c50f333ffec1")
+                .queryParam("key", trelloAppKey)
+                .queryParam("token", treloToken)
+                .queryParam("fields", "name,id")
+                .build()
+                .encode()
+                .toUri();
+
+        TrelloBoardDto[] boardsResponse = restTemplate.getForObject(url, TrelloBoardDto[].class);
+
+        return Optional.ofNullable(boardsResponse)
+                .map(Arrays::asList)
+                .orElse(Collections.emptyList());
+    }
+}
