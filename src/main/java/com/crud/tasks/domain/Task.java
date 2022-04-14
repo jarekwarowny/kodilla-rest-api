9f4fa5086cad86f6541499489fe7422c505653f3
Jarek Warowny <jwarowny99@gmail.com>
2022-04-14 19:58:31 +0200

    strategy

--- src/main/java/com/crud/tasks/domain/Task.java
+++ src/main/java/com/crud/tasks/domain/Task.java
@@ -14,7 +14,7 @@ import javax.validation.constraints.NotNull;
 public class Task {
 
     @Id
-    @GeneratedValue
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     @NotNull
     private Long id;
 
